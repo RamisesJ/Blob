9aaf03c4cd0459df3448f16aa3d52ce4f966cd38
Gantzul <ramises229@gmail.com>
2023-11-30 11:42:00 -0300

    Essa é minha nova tentativa

---
+++ src/Pet.java
@@ -0,0 +1,10 @@
+public class Pet {
+    public String nome;
+    public String raca;
+    public String tamanho;
+    public String sexo;
+    public String cor;
+    public String idade;
+    public String historia;
+//    public byte [] imagem;
+}
---
+++ src/TelaDeCadastro.java
@@ -0,0 +1,129 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+public class TelaDeCadastro extends JDialog {
+    private JTextField tf_cor;
+    private JTextField tf_name;
+    private JTextField tf_tamanho;
+    private JTextField tf_sexo;
+    private JTextField tf_raca;
+    private JTextField tf_idade;
+    private JTextField tf_historia;
+    private JLabel showImagem;
+    private JButton btUploadImage;
+    private JButton btCancelar;
+    private JButton btSalvar;
+    private JPanel tela;
+
+    public TelaDeCadastro(JFrame parent) {
+        super(parent);
+
+        setTitle("Cadastro de Pets");
+        setContentPane(tela);
+        setMinimumSize(new Dimension(650, 674));
+        setModal(true);
+        setLocationRelativeTo(parent);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+
+        btSalvar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cadastroPet();
+            }
+        });
+        btCancelar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+
+        setVisible(true);
+    }
+
+    private void cadastroPet() {
+        String nome = tf_name.getText();
+        String raca = tf_raca.getText();
+        String tamanho = tf_tamanho.getText();
+        String sexo = tf_sexo.getText();
+        String cor = tf_cor.getText();
+        String idade = tf_idade.getText();
+        String historia = tf_historia.getText();
+//        byte [] imagem =;
+        if (nome.isEmpty() || raca.isEmpty() || tamanho.isEmpty() || sexo.isEmpty() || cor.isEmpty() || historia.isEmpty() ){
+            JOptionPane.showConfirmDialog(this, "Preencha todas as informações", "Tente novamente", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        pet = inserirPetNoBanco (nome, raca, tamanho, sexo, cor, idade, historia);
+        if(pet != null){
+            dispose();
+        } else {
+            JOptionPane.showMessageDialog(this, "Não consegui registra o Pet no banco de dados", "Tente novamente", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public Pet pet;
+    private Pet inserirPetNoBanco(String nome, String raca, String tamanho, String sexo, String cor, String idade, String historia) {
+        Pet pet = null;
+        final String DB_URL = "jdbc:mysql://localhost:3306/pets";
+        final String USERNAME = "root";
+        final String PASSWORD = "Ramises229@";
+        try{
+            Connection con = DriverManager.getConnection(DB_URL,USERNAME,PASSWORD);
+            try {
+//                Blob imagemBlob = con.createBlob();
+//                imagemBlob.setBytes(1, pets.getImagem());
+                //Aqui já é a inserçao)
+                PreparedStatement pstmt = con.prepareStatement("INSERT INTO pets (nome, raca, tamanho, sexo, cor, idade, historia) VALUES (?, ?, ?, ?, ?, ?, ?) ");
+                pstmt.setString(1, nome);
+                pstmt.setString(2, raca);
+                pstmt.setString(3, tamanho);
+                pstmt.setString(4, sexo);
+                pstmt.setString(5, cor);
+                pstmt.setString(6, idade);
+                pstmt.setString(7, historia);
+//                pstmt.setBlob(8, imagemBlob);
+
+                int addedRows = pstmt.executeUpdate();
+                if (addedRows >0){
+                    pet = new Pet();
+                    pet.nome = nome;
+                    pet.raca = raca;
+                    pet.tamanho = tamanho;
+                    pet.sexo = sexo;
+                    pet.cor = cor;
+                    pet.idade = idade;
+                    pet.historia = historia;
+                }
+            } finally {
+                con.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return pet;
+    }
+
+
+    public static void main(String[] args) {
+        TelaDeCadastro telaDeCadastro = new TelaDeCadastro(null);
+        Pet pet = telaDeCadastro.pet;
+        if (pet != null){
+            System.out.println("Sucesso em registrar o Pet");
+        } else {
+            System.out.println("Registro cancelado");
+        }
+    }
+
+}
+
+
